facd1a70f83ac6ca998a98f9be935b5b04bfbefa
Prasoon <prasoonjadon.pis6@gmail.com>
2025-06-27 10:20:52 +0530

    Update app.js

--- template/app.js
+++ template/app.js
@@ -2,7 +2,37 @@
 import { createElement, render, createRouter } from 'aarambhjs';
 
 // Create a simple virtual DOM node
-const vdom = createElement('h1', { style: 'color: blue' }, 'Hello World from SnapUI!');
+function Home() {
+    return createElement("div", null,
+      createElement("h1", null, "Welcome to Aarambh.js!"),
+      createElement("button", {
+        onClick: () => router.navigate("/counter")
+      }, "Go to Counter")
+    );
+  }
 
-// Render and append to DOM
-document.getElementById('app').appendChild(render(vdom));
+  function Counter() {
+    let count = 0;
+    let countRef;
+
+    return createElement("div", null,
+      createElement("h1", null, "Counter"),
+      createElement("p", {
+        ref: el => countRef = el
+      }, "Count: 0"),
+      createElement("button", {
+        onClick: () => {
+          count++;
+          countRef.textContent = "Count: " + count;
+        }
+      }, "Increment"),
+      createElement("button", {
+        onClick: () => router.navigate("/")
+      }, "Back")
+    );
+  }
+
+  const router = createRouter({
+    '/': Home,
+    '/counter': Counter,
+  }, 'app');
